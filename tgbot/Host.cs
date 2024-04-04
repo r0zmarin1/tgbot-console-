29e993466a0fba2e846492ecabf8f353a591c1c4
AA <Student@CAB4-2>
2024-04-04 11:27:04 +1000

    Добавьте файлы проекта.

---
+++ tgbot/Host.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace tgbot
+{
+    public class Host
+    {
+        public Action<ITelegramBotClient, Update>? OnMessage;
+        
+        private TelegramBotClient kissabot;
+        public Host(string token)
+        {
+            kissabot = new TelegramBotClient(token);
+
+        }
+
+        public void Start()
+        {
+            kissabot.StartReceiving(Update, Error);
+            Console.WriteLine("Start!");
+        }
+
+        private async Task Update(ITelegramBotClient client, Update update, CancellationToken token)
+        {
+            Console.WriteLine($"New message: {update.Message?.Text ?? "[не текст]"}");
+            OnMessage?.Invoke(client, update);
+            await Task.CompletedTask;
+        }
+
+        private async Task Error(ITelegramBotClient client, Exception exception, CancellationToken token)
+        {
+            Console.WriteLine("Error:" + exception.Message);
+            await Task.CompletedTask;
+        }
+    }
+}
---
+++ tgbot/Program.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Threading;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.ReplyMarkups;
+using tgbot;
+
+internal class Program
+{
+    static CancellationToken token = new CancellationToken();
+    private static void Main()
+    {
+        Host kissabot = new Host("6888221290:AAH4uGzCC-9enq2KpdS0Iaigdiot4UHDv-Y");
+        kissabot.Start();
+        kissabot.OnMessage += OnMessage;
+        Console.ReadLine();
+    }
+
+    private static async void OnMessage(ITelegramBotClient client, Update update)
+    {
+        if (update.Message?.Text == "прив")
+        {
+            await client.SendTextMessageAsync(update.Message?.Chat.Id ?? 833690650, "привки");
+        }
+        ReplyKeyboardMarkup replyKeyboardMarkup = new(new[] {new KeyboardButton[] { "Help me" }, new KeyboardButton[] { "Call me" },})
+        {
+            ResizeKeyboard = true
+        };
+
+        
+        Message sentMessage = await client.SendTextMessageAsync(update.Message?.Chat.Id, "Choose a response", replyMarkup: replyKeyboardMarkup, cancellationToken: token );
+
+
+    }
+}
