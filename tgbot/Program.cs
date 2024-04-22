d09f27aad8a37745456beb795e09b085e90587b2
AA <Student@CAB4-2>
2024-04-23 09:48:52 +1000

    hhgbjhgj

--- tgbot/Program.cs
+++ tgbot/Program.cs
@@ -1,14 +1,21 @@
 ﻿using Newtonsoft.Json.Linq;
+using System.Data.Common;
 using System.Threading;
 using Telegram.Bot;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.ReplyMarkups;
 using tgbot;
 
+class LastMessage
+{
+    public IReplyMarkup replyMarkup;
+    public string file;
+    public string text;
+}
 internal class Program
 {
     static CancellationToken token = new CancellationToken();
-    static Host kissabot;
+    static Host? kissabot;
     private static void Main()
     {
         kissabot = new Host("6888221290:AAH4uGzCC-9enq2KpdS0Iaigdiot4UHDv-Y");
@@ -16,14 +23,32 @@ internal class Program
         kissabot.OnMessage += OnMessage;
         Console.ReadLine();
     }
+    static Dictionary<long, LastMessage> lastButtons = new();
 
+    static LastMessage SetLastButtons(long user, IReplyMarkup buttons, string file, string text)
+    {
+        var data = new LastMessage { replyMarkup = buttons, file = file, text = text };
+        if (lastButtons.ContainsKey(user))
+            lastButtons[user] = data;
+        else
+            lastButtons.Add(user, data);
+        return data;
+    }
+    static ReplyKeyboardMarkup replyKeyboardMarkup = new(new[] { new KeyboardButton[] { "новый заказ" }, new KeyboardButton[] { "меню" }, new KeyboardButton[] { "статус заказа" } })
+    {
+        ResizeKeyboard = true
+    };
+    static LastMessage GetLastButtons(long user)
+    {
+        if (lastButtons.ContainsKey(user))
+            return lastButtons[user];
+        else
+        {
+            return new LastMessage { replyMarkup = replyKeyboardMarkup , file = "https://raw.githubusercontent.com/r0zmarin1/tgbot-console-/master/tgbot/docs/greeting_photo.jpeg", text = "На связи Кисса-бот!\nВыбери нужную команду;)" };
+        }
+    }
     private static async void OnMessage(ITelegramBotClient client, Update update)
     {
-
-        ReplyKeyboardMarkup replyKeyboardMarkup = new(new[] { new KeyboardButton[] { "новый заказ" }, new KeyboardButton[] { "меню" }, new KeyboardButton[] { "статус заказа" } })
-        {
-            ResizeKeyboard = true
-        };
 
         InlineKeyboardMarkup newOrder = new(new[]
         {
@@ -43,23 +68,32 @@ internal class Program
         {
             new[]
             {
-            InlineKeyboardButton.WithCallbackData(text: "кофя", callbackData: "coffee"),
-            InlineKeyboardButton.WithCallbackData(text: "иное", callbackData: "another"),
+                InlineKeyboardButton.WithCallbackData(text: "кофя", callbackData: "coffee"),
+                InlineKeyboardButton.WithCallbackData(text: "иное", callbackData: "another"),
             },
-        }) ;
+            new[]
+            {
+                InlineKeyboardButton.WithCallbackData(text: "назад", callbackData: "toBack"),
+            },
+        });
 
         InlineKeyboardMarkup adds = new(new[]
         {
             new[]
             {
-            InlineKeyboardButton.WithCallbackData(text: "альтернативное молоко", callbackData: "alternative_milk"),
-            InlineKeyboardButton.WithCallbackData(text: "сироп", callbackData: "syrup"),
+                InlineKeyboardButton.WithCallbackData(text: "альтернативное молоко", callbackData: "alternative_milk"),
+                InlineKeyboardButton.WithCallbackData(text: "сироп", callbackData: "syrup"),
             },
 
             new[]
             {
-            InlineKeyboardButton.WithCallbackData(text: "авторский напиток", callbackData: "author_drinks"),
-            InlineKeyboardButton.WithCallbackData(text: "выпечка", callbackData: "bakery"),
+                InlineKeyboardButton.WithCallbackData(text: "авторский напиток", callbackData: "author_drinks"),
+                InlineKeyboardButton.WithCallbackData(text: "выпечка", callbackData: "bakery"),
+            },
+
+            new[]
+            {
+                InlineKeyboardButton.WithCallbackData(text: "назад", callbackData: "toBack"),
             },
         });
 
@@ -67,27 +101,39 @@ internal class Program
         {
             new[]
             {
-            InlineKeyboardButton.WithCallbackData(text: "кофя", callbackData: "special_coffee"),
-            InlineKeyboardButton.WithCallbackData(text: "не кофя", callbackData: "another_coffee"),
+                InlineKeyboardButton.WithCallbackData(text: "кофя", callbackData: "special_coffee"),
+                InlineKeyboardButton.WithCallbackData(text: "не кофя", callbackData: "another_coffee"),
+            },
+            new[]
+            {
+                InlineKeyboardButton.WithCallbackData(text: "назад", callbackData: "toBack"),
             },
         });
-        await kissabot.Delete(client, update, token);
+        string file, text;
+        LastMessage data = null;
         switch (update.CallbackQuery?.Data)
         {
             case "toBack":
-                await client.SendPhotoAsync(update.CallbackQuery.From.Id, InputFile.FromUri("https://raw.githubusercontent.com/r0zmarin1/tgbot-console-/master/tgbot/docs/greeting_photo.jpeg"), caption: "На связи Кисса-бот!\nВыбери нужную команду;)", replyMarkup: replyKeyboardMarkup, cancellationToken: token);
+                data = GetLastButtons(update.CallbackQuery.From.Id);
                 break;
             case "base_menu":
-                await client.SendPhotoAsync(update.CallbackQuery.From.Id, InputFile.FromUri("https://raw.githubusercontent.com/r0zmarin1/tgbot-console-/master/tgbot/docs/base_menu.jpg"), caption: "супер! что дальше?", replyMarkup: base_menu, cancellationToken: token);
+                file = "https://raw.githubusercontent.com/r0zmarin1/tgbot-console-/master/tgbot/docs/base_menu.jpg";
+                text = "супер! что дальше?";
+                data = SetLastButtons(update.CallbackQuery.From.Id, base_menu, file, text);
                 break;
             case "adds":
-                await client.SendPhotoAsync(update.CallbackQuery.From.Id, InputFile.FromUri("https://raw.githubusercontent.com/r0zmarin1/tgbot-console-/master/tgbot/docs/adds.png"), caption: "супер! что дальше?", replyMarkup: adds, cancellationToken: token);
+                file = "https://raw.githubusercontent.com/r0zmarin1/tgbot-console-/master/tgbot/docs/adds.png";
+                text = "супер! что дальше?";
+                data = SetLastButtons(update.CallbackQuery.From.Id, adds, file, text);
                 break;
             case "special_menu":
-               var mes =  await client.SendPhotoAsync(update.CallbackQuery.From.Id, InputFile.FromUri("https://raw.githubusercontent.com/r0zmarin1/tgbot-console-/master/tgbot/docs/special.png"), caption: "супер! что дальше?", replyMarkup: special_menu, cancellationToken: token);
-                kissabot.lastMessageId = mes.MessageId;
+                file = "https://raw.githubusercontent.com/r0zmarin1/tgbot-console-/master/tgbot/docs/special.png";
+                text = "супер! что дальше?";
+                data = SetLastButtons(update.CallbackQuery.From.Id, special_menu, file, text);
                 break;
         }
+        if (data != null)
+            await client.SendPhotoAsync(update.CallbackQuery.From.Id, InputFile.FromUri(data.file), caption: data.text, replyMarkup: data.replyMarkup, cancellationToken: token);
 
         switch (update.Message?.Text?.ToLower())
         {
@@ -95,8 +141,7 @@ internal class Program
                 await client.SendPhotoAsync(update.Message.Chat.Id, InputFile.FromUri("https://raw.githubusercontent.com/r0zmarin1/tgbot-console-/master/tgbot/docs/greeting_photo.jpeg"), caption: "На связи Кисса-бот!\nВыбери нужную команду;)", replyMarkup: replyKeyboardMarkup, cancellationToken: token);
                 break;
             case "новый заказ":
-                var mes = await client.SendPhotoAsync(update.Message.Chat.Id, InputFile.FromUri("https://raw.githubusercontent.com/r0zmarin1/tgbot-console-/master/tgbot/docs/menu.jpeg"), caption: "глянь меню и выбери категорию", replyMarkup: newOrder, cancellationToken: token);
-                kissabot.lastMessageId = mes.MessageId;
+                await client.SendPhotoAsync(update.Message.Chat.Id, InputFile.FromUri("https://raw.githubusercontent.com/r0zmarin1/tgbot-console-/master/tgbot/docs/menu.jpeg"), caption: "глянь меню и выбери категорию", replyMarkup: newOrder, cancellationToken: token);
                 break;
             case "меню":
                 await client.SendPhotoAsync(update.Message.Chat.Id, InputFile.FromUri("https://raw.githubusercontent.com/r0zmarin1/tgbot-console-/master/tgbot/docs/menu.jpeg"), replyMarkup: replyKeyboardMarkup, cancellationToken: token);
@@ -107,7 +152,7 @@ internal class Program
             case "22":
                 await client.SendPhotoAsync(update.Message.Chat.Id, InputFile.FromUri("https://raw.githubusercontent.com/r0zmarin1/tgbot-console-/master/tgbot/docs/greeting_photo.jpeg"), caption: "На связи Кисса-бот!\nВыбери нужную команду;)", replyMarkup: replyKeyboardMarkup, cancellationToken: token);
                 break;
-            
+
         }
 
 
